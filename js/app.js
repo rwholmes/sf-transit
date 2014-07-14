28c21449a7e0621e29da2c79e5e7baa9b7e901be
Robert Holmes <rwholmes1@gmail.com>
2014-07-14 11:49:17 -0700

    Basic parsing of nextBus stops and display on map

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ var m_width = $('#map').width();
 var width  = 938;
 var height = 500;
 var offset = [width/2, height/1.7];
+var zoomed = 0;
 
 var projection = d3.geo.mercator()
     .scale(160000)
@@ -23,13 +24,12 @@ svg.append("rect")
     .attr("class", "background")
     .attr("width", width)
     .attr("height", height)
-    .on("click", mapClicked);
+    .on("click", mapClicked)
+    .on("dblclick", zoomOut);
 
 var g = svg.append("g");
 
 d3.json("maps/neighborhoods.json", function(json) {
-  console.log('json ', json);
-
   g.append("g")
     .attr("id", "neighborhoods")
     .selectAll("path")
@@ -38,7 +38,8 @@ d3.json("maps/neighborhoods.json", function(json) {
     .append("path")
     .attr("id", function(d) { return d.properties.neighborho; })
     .attr("d", path)
-    .on("click", mapClicked);
+    .on("click", mapClicked)
+    .on("dblclick", zoomOut);
 
   // svg.selectAll("path").data(json.features).enter().append("path")
   //   .attr("d", path)
@@ -49,12 +50,12 @@ d3.json("maps/neighborhoods.json", function(json) {
 });
 
 var zoom = function(xyz) {
-  console.log('zooming');
-  svg.transition()
+  console.log('zoomed ', xyz);
+  zoomed = xyz;
+  g.transition()
     .duration(750)
-    .attr("transform", "translate(" + projection.translate() + ")scale(" + xyz[2] + ")translate(-" + xyz[0] + ",-" + xyz[1] + ")")
-    .style("stroke-width", 1.0 / xyz[2] + "px");
-};
+    .attr("transform", "translate(" + projection.translate() + ")scale(" + xyz[2] + ")translate(-" + xyz[0] + ",-" + xyz[1] + ")"); 
+}
 
 var getXyz = function(d) {
   var bounds = path.bounds(d);
@@ -72,6 +73,61 @@ var mapClicked = function(d) {
   var xyz = getXyz(d);
   zoom(xyz);
 };
+
+var zoomOut = function() {
+  var outX = zoomed[0];
+  var outY = -zoomed[1];
+
+  g.transition()
+  .duration(750)
+  .attr("transform", "translate(" + projection.translate() + ")scale(1)translate(-" + outX + ",-" + outY + ")");
+}
+
+
+/// CODE FROM OTHER FILE
+
+$(document).ready(function() {
+  $.ajax({
+    url: 'http://webservices.nextbus.com/service/publicXMLFeed?command=routeConfig&a=sf-muni&r=N',
+    type: 'GET',
+    dataType: 'xml',
+    success: function(xml) {
+      var stops = [];
+      var $route = $(xml).find('route')[0];
+      for (var j = 0; j < $route.childNodes.length; j++) {
+        var stop = $route.childNodes[j];
+        if (stop.tagName == 'stop') {
+          var parsedStop = {
+            tag: stop.getAttribute('tag'),
+            title: stop.getAttribute('title'),
+            lat: stop.getAttribute('lat'),
+            lon: stop.getAttribute('lon')
+          };
+          stops.push(parsedStop);
+        }
+      }
+        
+
+      g.selectAll("circle")
+        .data(stops)
+        .enter()
+        .append("circle")
+        .attr("cx", function(d) {
+          return projection([d.lon, d.lat])[0];
+        })
+        .attr("cy", function(d) {
+          return projection([d.lon, d.lat])[1];
+        })
+        .attr("r", 3)
+        .style("fill", "black");
+    }
+  });
+});
+
+
+
+
+
 
 
 
